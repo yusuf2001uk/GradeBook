638923ac872ecda7c0f5b96a98779a5f0ce881bd
Yusuf Othman <yusuf2001uk@gmail.com>
2020-07-26 22:47:04 +0300

    4th Gradeabook commit

--- Program.cs
+++ Program.cs
@@ -1,24 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-
 namespace GradeBook
 {
-    class Program
+       class Program
     {
         static void Main(string[] args)
         {
-
-         var grades = new List<double>(){99.9,10.1,23.4} ;
+         if (true){
+         var grades = new List<double>() {99.9,10.1,23.4} ;
          double sum = 0.0;
          double avg = 0.0; 
          foreach( double number in grades)
          {
-             System.Console.WriteLine(grades);
+             System.Console.WriteLine(number);
              sum +=number;
-             }
-             Console.WriteLine($"the Sum is {sum}");
-             avg/= grades.Count;
-         System.Console.WriteLine($"The Average is {avg}");
+         }
+             avg=sum/grades.Count;
+             Console.WriteLine($"The Sum is {sum}");
+             Console.WriteLine($"The Average is {avg:N3}");
+            }
+            else{
+                Console.WriteLine("Error Input, Bye!");
+            }
         }
     }
 }
