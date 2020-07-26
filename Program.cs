e5c517d8acac722ab52518035286325a69865b4b
Yusuf Othman <yusuf2001uk@gmail.com>
2020-07-26 12:16:12 +0300

    1st Gradebook Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+         var numbers= new [] {99.9,10.1,23.4};
+         double result= 0.0;
+         foreach(double number in numbers)
+         {
+             System.Console.WriteLine(number);
+             result +=number;
+             }
+         System.Console.WriteLine(result);
+        }
+    }
+}
