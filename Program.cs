54a0e334c14e93fdd7305caaf8035c9111b3c04f
Yusuf Othman <yusuf2001uk@gmail.com>
2020-08-02 19:30:55 +0300

    5th Gradebook commit

--- Program.cs
+++ Program.cs
@@ -6,22 +6,12 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-         if (true){
-         var grades = new List<double>() {99.9,10.1,23.4} ;
-         double sum = 0.0;
-         double avg = 0.0; 
-         foreach( double number in grades)
-         {
-             System.Console.WriteLine(number);
-             sum +=number;
-         }
-             avg=sum/grades.Count;
-             Console.WriteLine($"The Sum is {sum}");
-             Console.WriteLine($"The Average is {avg:N3}");
-            }
-            else{
-                Console.WriteLine("Error Input, Bye!");
-            }
+         Book book = new Book("Joseph's Grade Book");
+         book.AddGrade(89.1);
+         book.AddGrade(90.5);
+         book.AddGrade(77.5);
+         book.ShowStatistics();
+
         }
     }
 }
