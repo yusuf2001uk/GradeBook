3a7f4b7a1b45b9879baddbe15fbbd3706e0fe939
Yusuf Othman <yusuf2001uk@gmail.com>
2020-08-10 20:23:00 +0300

    12th GradeBook Commit

--- Program.cs
+++ Program.cs
@@ -2,21 +2,27 @@
 using System.Collections.Generic;
 namespace GradeBook
 {
-       class Program
+  class Program
     {
         static void Main(string[] args)
         {
-         Book book = new Book("Yusuf GradeBook");
-         book.AddGrade(89.1);
-         book.AddGrade(90.5);
-         book.AddGrade(77.5);
-         
-        var stats=book.GetStatistics();
-
-        Console.WriteLine($"The lowest grade is {stats.Low}");
-        Console.WriteLine($"The highest grade is {stats.High}");
-        Console.WriteLine($"The Average is {stats.Average:N3}");
-        Console.WriteLine($"The Letter Grade is {stats.letter}");
+         Book book = new Book("Student GradeBook");
+         var done = false;        
+         while (!done){
+         Console.WriteLine("Enter a grade or 'q' to quit");   
+         var input = Console.ReadLine();
+         if (input=="q"){
+             done = true;
+             continue;
+         }
+         var grade = double.Parse(input);
+         book.AddGrade(grade);   
+        }         
+         var stats=book.GetStatistics();
+         Console.WriteLine($"The lowest grade is {stats.Low}");
+         Console.WriteLine($"The highest grade is {stats.High}");
+         Console.WriteLine($"The Average is {stats.Average:N3}");
+         Console.WriteLine($"The Letter Grade is {stats.letter}");
         }
     }
 }
